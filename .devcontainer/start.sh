3eded9678d33cbf259081b15f50e59a21f7f44a6
Andres Felipe Barreto <afelipebarretoj@javeriana.edu.co>
2021-09-08 08:37:24 -0500

    subiendo archivos nvidia

--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -1,6 +1,7 @@
 xhost +
 docker build -t ros_kinetic_xenial_1.0 .
-docker run --rm -it \
+docker run --gpus all --rm -it \
+    --runtime=nvidia\
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v /dev/dri:/dev/dri \
     -e DISPLAY=$DISPLAY \
